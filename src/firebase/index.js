71963c53cdd1953d3039273beae0267491002b56
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-05-15 18:48:58 +0300

    some changes

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -14,16 +14,15 @@ import {doc, getFirestore, setDoc,getDoc,collection,addDoc,getDocs,query,orderBy
 import store from "../store/index.js";
 
 const firebaseConfig={
-    apiKey:process.env.FIREBASE_API_KEY,
-    authDomain:process.env.FIREBASE_AUTH_DOMAIN,
-    projectId:process.env.FIREBASE_PROJECT_ID,
-    storageBucket:process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId:process.env.FIREBASE_APP_ID,
-    measurementId:process.env.FIREBASE_MEASUREMENT_ID
+    apiKey:`${process.env.FIREBASE_API_KEY}`,
+    authDomain:`${process.env.FIREBASE_AUTH_DOMAIN}`,
+    projectId:`${process.env.FIREBASE_PROJECT_ID}`,
+    storageBucket:`${process.env.FIREBASE_STORAGE_BUCKET}`,
+    messagingSenderId:`${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
+    appId:`${process.env.FIREBASE_APP_ID}`,
+    measurementId:`${process.env.FIREBASE_MEASUREMENT_ID}`
 
 }
-console.log(process.env.TEST);
 
 
 
