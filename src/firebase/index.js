5dfadfb12eec45fe54ac44bf8defb3890663f0a9
Yunus Emre Arslan <75901751+earslanyunus@users.noreply.github.com>
2023-07-09 00:48:05 +0300

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -28,7 +28,7 @@ export const firebaseConfig={
 
 
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
 const auth = getAuth(app)
 const db = getFirestore(app)
 const storage = getStorage()
